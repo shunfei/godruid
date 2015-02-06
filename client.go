f9cfbb6facdcadc4ad8b633ee9665165f084c388
Martin Karlsch <martin@karlsch.com>
2015-02-06 18:50:39 +0100

    make Query interface public so we can implement it outside of this package

--- client.go
+++ client.go
@@ -1,77 +1,77 @@
 package godruid
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const (
-    DefaultEndPoint = "/druid/v2"
+	DefaultEndPoint = "/druid/v2"
 )
 
 type Client struct {
-    Url      string
-    EndPoint string
+	Url      string
+	EndPoint string
 
-    Debug        bool
-    LastRequest  string
-    LastResponse string
+	Debug        bool
+	LastRequest  string
+	LastResponse string
 }
 
 func (c *Client) Query(query Query) (err error) {
-    query.setup()
-    var reqJson []byte
-    if c.Debug {
-        reqJson, err = json.MarshalIndent(query, "", "  ")
-    } else {
-        reqJson, err = json.Marshal(query)
-    }
-    if err != nil {
-        return
-    }
-    result, err := c.QueryRaw(reqJson)
-    if err != nil {
-        return
-    }
+	query.Setup()
+	var reqJson []byte
+	if c.Debug {
+		reqJson, err = json.MarshalIndent(query, "", "  ")
+	} else {
+		reqJson, err = json.Marshal(query)
+	}
+	if err != nil {
+		return
+	}
+	result, err := c.QueryRaw(reqJson)
+	if err != nil {
+		return
+	}
 
-    return query.onResponse(result)
+	return query.OnResponse(result)
 }
 
 func (c *Client) QueryRaw(req []byte) (result []byte, err error) {
-    if c.EndPoint == "" {
-        c.EndPoint = DefaultEndPoint
-    }
-    endPoint := c.EndPoint
-    if c.Debug {
-        endPoint += "?pretty"
-        c.LastRequest = string(req)
-    }
-    if err != nil {
-        return
-    }
+	if c.EndPoint == "" {
+		c.EndPoint = DefaultEndPoint
+	}
+	endPoint := c.EndPoint
+	if c.Debug {
+		endPoint += "?pretty"
+		c.LastRequest = string(req)
+	}
+	if err != nil {
+		return
+	}
 
-    resp, err := http.Post(c.Url+endPoint, "application/json", bytes.NewBuffer(req))
-    if err != nil {
-        return
-    }
-    defer func() {
-        resp.Body.Close()
-    }()
+	resp, err := http.Post(c.Url+endPoint, "application/json", bytes.NewBuffer(req))
+	if err != nil {
+		return
+	}
+	defer func() {
+		resp.Body.Close()
+	}()
 
-    result, err = ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return
-    }
-    if c.Debug {
-        c.LastResponse = string(result)
-    }
+	result, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	if c.Debug {
+		c.LastResponse = string(result)
+	}
 
-    if resp.StatusCode != http.StatusOK {
-        return nil, fmt.Errorf("%s: %s", resp.Status, string(result))
-    }
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s: %s", resp.Status, string(result))
+	}
 
-    return
+	return
 }
