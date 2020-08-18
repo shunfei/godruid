c6408fc8976504e6a51873bbe63c529aada2d274
Sameer Ladiwala <sladiwal@cisco.com>
2020-08-18 07:08:03 -0700

    address review comments
    provided a link to the documentation.

--- extraction_function.go
+++ extraction_function.go
@@ -1,5 +1,7 @@
 package godruid
 
+// See https://druid.apache.org/docs/latest/querying/dimensionspecs.html#extraction-functions
+// for details.
 type ExtractionFn struct {
 	Type string `json:"type"`
 }
