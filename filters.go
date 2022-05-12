928b9aeff39f69bfb05e31c932eb07e818052a08
ndkcha <anand.kacha@unity3d.com>
2022-05-12 13:45:46 -0700

    Add Search Filter

--- filters.go
+++ filters.go
@@ -9,6 +9,15 @@ type Filter struct {
 	Field        *Filter             `json:"field,omitempty"`
 	Fields       []*Filter           `json:"fields,omitempty"`
 	ExtractionFn *FilterExtractionFn `json:"extractionFn,omitempty"`
+	QueryFilter  *Query              `json:"query,omitempty"`
+	Values       []string            `json:"values,omitempty"`
+}
+
+// QueryFilter is used for search filter
+// https://druid.apache.org/docs/latest/querying/filters.html
+type QueryFilter struct {
+	Type  string `json:"type"`
+	Value string `json:"value"`
 }
 
 type FilterExtractionFn struct {
